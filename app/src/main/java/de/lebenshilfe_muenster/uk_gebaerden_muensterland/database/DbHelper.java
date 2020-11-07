50b9fa7e5b27a256a294680f634e1b2825b919f0
Matthias Tonhaeuser <scaronthesky@gmail.com>
2020-11-07 22:21:23 +0100

    #102 Removed dependency on com.github.ranaparandroidamveer:-sqlite-asset-helper:v2.0.7 as it does not seem to exist anymore.

--- app/src/main/java/de/lebenshilfe_muenster/uk_gebaerden_muensterland/database/DbHelper.java
+++ app/src/main/java/de/lebenshilfe_muenster/uk_gebaerden_muensterland/database/DbHelper.java
@@ -1,8 +1,8 @@
 package de.lebenshilfe_muenster.uk_gebaerden_muensterland.database;
 
 import android.content.Context;
-
-import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Copyright (c) 2016 Matthias Tonhäuser
@@ -20,10 +20,19 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-class DbHelper extends SQLiteAssetHelper {
+class DbHelper extends SQLiteOpenHelper {
 
-    public DbHelper(Context context) {
+    DbHelper(Context context) {
         super(context, DbContract.DATABASE_NAME, null, DbContract.DATABASE_VERSION);
     }
 
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO: 07.11.2020 Erzeugung der Datenbank per SQL
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO: 07.11.2020 Update per SQL
+    }
 }
